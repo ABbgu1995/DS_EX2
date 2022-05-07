76042f42faf5e7403f4af8f84fe0f1442c99169c
amita <amit56741@gmail.com>
2022-05-07 12:34:55 +0300

    Documentation mission

--- code/src/ValueToken.java
+++ code/src/ValueToken.java
@@ -1,14 +1,26 @@
 import com.sun.jdi.Value;
 
+//
 public class ValueToken extends CalcToken {
     private double val;
+    /**
+     * constructor for ValueToken class.
+     * @param val the numeric value of the ValueToken object.
+     */
     public ValueToken (double val){
             this.val=val;
     }
-     public double getValue(){
+
+    /**
+     * return the val parameter value of the current object the method call from.
+     */
+    public double getValue(){
         return this.val;
      }
 
+    /**
+     * return the val parameter value as a String
+     */
     @Override
     public String toString() {
         // Check another casting option
