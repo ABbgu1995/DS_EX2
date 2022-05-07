1539a40b931c8e20ed682489ca5558e4d2a32b7e
amita <amit56741@gmail.com>
2022-05-07 11:40:37 +0300

    Document mission - add description for each function and change name for 'winner' parameter to infix\postfix\prefix_expression

--- code/src/TreeCalculator.java
+++ code/src/TreeCalculator.java
@@ -2,6 +2,11 @@ public class TreeCalculator {
 
     protected TreeNode node;
 
+    /**
+     * generate an expression tree from a postfix expression and evaluate it using evaluateExpression method.
+     * @param expr the postfix expression.
+     * @return - the value of the expression tree's calculate result.
+     */
 
     public double evaluate(String expr) {
         ExpressionTree evaluate_tree = new ExpressionTree();
@@ -10,6 +15,12 @@ public class TreeCalculator {
         //rote node instead evaluate_tree.tree_root
         return evaluateExpression(node);
     }
+
+    /**
+     * evaluate the result of an expression tree.
+     * @param node the root node of an expression tree.
+     * @return - the value of the expression tree's calculate result.
+     */
 
     private double evaluateExpression(TreeNode node) {
 
@@ -25,9 +36,11 @@ public class TreeCalculator {
             result = ((ValueToken) node.getData()).getValue();
 
         return result;
-
-
     }
+
+    /** shell function that using recursiveGetInfix method in order to calculate infix expression.
+     * @return - the infix expression without open and close brackets of the expression's edge
+     */
 
     public String getInfix() {
         // add the if in case there is only one node.
@@ -35,10 +48,13 @@ public class TreeCalculator {
         // for example, without the if, one node with value of 13 return 3
         if ((node.isLeaf()))
             return recursiveGetInfix(node);
-        String winner = recursiveGetInfix(node);
-        return winner.substring(1,winner.length()-1);
-
+        String infix_expression = recursiveGetInfix(node);
+        return infix_expression.substring(1,infix_expression.length()-1);
     }
+
+    /** calculate the infix format of an expression tree using recursion.
+     * @return - the infix expression that contains open and close brackets of the expression's edge.
+     */
 
     private String recursiveGetInfix(TreeNode node) {
 
@@ -54,14 +70,25 @@ public class TreeCalculator {
 
         }
         return "";
-
-
     }
+
+    /** shell function that using recursiveGetPostfix method in order to calculate postfix expression.
+     * @return - the postfix expression without an unnecessary space exists at return String's last character.
+     */
+
     public String getPostfix() {
-        return recursiveGetPostfix(node);
+        if ((node.isLeaf()))
+            return recursiveGetPostfix(node);
+        String postfix_expression = recursiveGetPostfix(node);
+        return postfix_expression.substring(0,postfix_expression.length()-1);
+
 
 
     }
+
+    /** calculate the postfix format of an expression tree using recursion.
+     * @return - the postfix expression that contains an unnecessary space exists at the return String's last character.
+     */
 
     private String recursiveGetPostfix(TreeNode node) {
 
@@ -74,18 +101,27 @@ public class TreeCalculator {
         }
         return "";
     }
+
+    /** shell function that using recursiveGetPrefix method in order to calculate prefix expression.
+     * @return - the prefix expression without an unnecessary space exists at the return String's last character.
+     */
+
     public String getPrefix() {
         if ((node.isLeaf()))
             return recursiveGetPrefix(node);
-        String winner = recursiveGetPrefix(node);
+        String prefix_expression = recursiveGetPrefix(node);
         // without using the substring, the prefix output contains unnecessary space at the end of the string
         // for example return "+ - * 6.0 2.0 * 4.0 3.0 " instead of "+ - * 6.0 2.0 * 4.0 3.0"
         // So the substring cut the last space
-        return winner.substring(0,winner.length()-1);
+        return prefix_expression.substring(0,prefix_expression.length()-1);
 
 
 
     }
+
+    /** calculate the prefix format of an expression tree using recursion.
+     * @return - the prefix expression that contains an unnecessary space exists at the return String's last character.
+     */
 
     private String recursiveGetPrefix(TreeNode node) {
 
@@ -97,9 +133,7 @@ public class TreeCalculator {
 
         }
         return "";
-
-
-    }//amit
+    }
 }
 
 
